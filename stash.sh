385e066016455503cb17056a3f3d8db5fd10d426
Andrew Phillips <andrew.phillips2@canada.ca>
2017-01-10 17:10:03 -0400

    Encrypt mostly working

--- stash.sh
+++ stash.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-OPTIONS="dek:c:s:d:"
+set -x
+
+OPTIONS="dek:c:D:"
 CIPHER=aes-256-gcm
 MODE=encrypt
-READ=-
-WRITE=-
 DIGEST=sha512
 
 if ( ! getopts $OPTIONS opt); then
   cat >&2 <<EOF
 Usage:
-  Encrypt: `basename $0` -e [-k key] [-c cipher] [-s key-size] [-D digest] [in] [out]
+  Encrypt: `basename $0` -e [-k key] [-c cipher] [-D digest] [in] [out]
   Decrypt: `basename $0` -d -k <key> [in] [out]
 EOF
   exit $E_OPTERROR;
@@ -21,7 +21,7 @@ while getopts $OPTIONS opt; do
     e) MODE=encrypt ;;
     d) MODE=decrypt ;;
     c) CIPHER=$OPTARG ;;
-    D) DIGEST=$OPTARG ;;
+    D) DIGEST=$OPTARG ;;    
     k) KEY=$OPTARG ;;
   esac
 done
@@ -30,38 +30,89 @@ shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
-echo "MODE: $MODE"
-echo "Args: $@"
-exit 0
+READ=$1
+WRITE=$2
 
-set -xe
+TMPDIR=$(mktemp -d /tmp/stash-XXXXXXXXXX)
+if [[ -z $READ ]] || [[ $READ = '-' ]]; then
+  READ=/dev/stdin
+fi
+
+if [[ -z $WRITE ]] || [[ $WRITE = '-' ]]; then
+  WRITE=/dev/stdout
+fi
 
 function stash_make_nonce() {
   SIZE=${1:-16}
-  RET=$(date +"%s %N" | awk '{printf "%08x%08x\n", $1, $2}' | cut -c 1-%SIZE)
+  RET=$(date +"%s %N" | awk '{printf "%08x%08x\n", $1, $2}' | cut -c 1-$SIZE)
 }
 
-CIPHER=aes-256-gcm
-SIZE=256
-SIZEB=$(( $SIZE / 8 / 2 ))
-KEY=$(openssl rand -hex $SIZEB)
-IV=
+function stash_digest() {
+  local DIGEST=$1
+  local FILE=$2
 
-TMPDIR=$(mktemp -d /tmp/stash-XXXXXXXXXX)
+  case $DIGEST in
+    sha|sha1|sha-1|SHA|SHA1|SHA-1)
+      RET=$(sha1sum $FILE | cut -f 1 -d ' ') ;;
+    
+    sha224|sha-224|SHA224|SHA-224)
+      RET=$(sha224sum $FILE | cut -f 1 -d ' ') ;;
+    
+    sha256|sha-256|SHA256|SHA-256)
+      RET=$(sha256sum $FILE | cut -f 1 -d ' ') ;;
 
+    sha384|sha-384|SHA384|SHA-384)
+      RET=$(sha384sum $FILE | cut -f 1 -d ' ') ;;
 
-cat > $TMPDIR/control <<EOF
+    sha512|sha-512|SHA512|SHA-512)
+      RET=$(sha512sum $FILE | cut -f 1 -d ' ') ;;
+
+    md5|MD5)
+      RET=$(md5sum $FILE | cut -f 1 -d ' ') ;;
+  esac
+}
+
+function stash_encrypt() {
+  echo "Read: $READ  Write: $WRITE"
+  
+  if [[ -z $KEY ]]; then
+    KEY=$(openssl rand -hex 16)
+  fi
+
+  stash_make_nonce
+  IV=$RET
+
+  cat $READ > $TMPDIR/unencrypted
+
+  stash_digest $DIGEST $TMPDIR/unencrypted
+  DIGEST_DATA=$RET
+
+  openssl $CIPHER -e -K $KEY -iv $IV < $TMPDIR/unencrypted > $TMPDIR/encrypted
+  rm -f $TMPDIR/unencrypted
+  
+  cat > $TMPDIR/control <<EOF
 IV: $IV
 Cipher: $CIPHER
 Digest: $DIGEST $DIGEST_DATA
 EOF
 
-openssl $CIPHER -e -K $KEY -iv $IV > $TMPDIR/data
+  echo "Key: $KEY" 1>&2
+  cat $TMPDIR/control 1>&2
+  
+  ar r $TMPDIR/stash $TMPDIR/control $TMPDIR/encrypted
+  cat $TMPDIR/stash > $WRITE
+}
 
-ar r $TMPDIR/stash $TMPDIR/control $TMPDIR/data
+function stash_decrypt() {
+  false
+}
 
-echo "Key: $KEY" 1>&2
-cat $TMPDIR/control 1>&2
+case $MODE in
+  encrypt) stash_encrypt ;;
+  decrypt) stash_decrypt ;;
+esac
 
 rm -rf $TMPDIR
+
+exit 0
 
